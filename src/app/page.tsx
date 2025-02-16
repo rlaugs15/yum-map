5878879f7c6ea0b90402a67b7dc77897d8226451
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-02-16 19:17:41 +0900

    feat: 지도, 맛집 섹션 반응형 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,15 @@ import NearbyEats from "@/features/home/NearbyEats/NearbyEats";
 
 export default function Home() {
   return (
-    <div>
-      <section className="w-full grid grid-cols-3 gap-4">
-        <KakaoMap classname="col-span-2" />
+    <div className="flex flex-col gap-6">
+      <section className="w-full grid web:grid-cols-3 mobile:grid-cols-1 gap-4">
+        <KakaoMap classname="web:col-span-2 mobile:h-[300px]" />
         <NearbyEats />
+      </section>
+      <section className="bg-white w-full px-4 py-2">
+        <header>
+          <h2 className="text-lg font-bold">추천 레시피</h2>
+        </header>
       </section>
     </div>
   );
